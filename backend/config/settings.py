7e038e65f5518f6c51857c6d1eb3fb9f57e00dd6
Ranga <ranganath6300@gmail.com>
2025-12-10 01:14:08 +0530

    Initial commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-v8=_s_v%g$138bio0(z0322%0=(u+oqbx$!^yv7okmg=vp5vmo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -66,19 +66,19 @@ MIDDLEWARE = [
 ROOT_URLCONF = "config.urls"
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#         "OPTIONS": {
+#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 TEMPLATES = [
     {
@@ -102,12 +102,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 REST_FRAMEWORK = {
@@ -141,23 +141,40 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    "https://military-asset-management-system-beta.vercel.app",
-] 
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://military--asset--management--system.vercel.app",
+    "https://military-asset-management-system-3-zd1d.onrender.com",
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://*.onrender.com",
+    "https://military--asset--management--system.vercel.app",
+    "https://military-asset-management-system-3-zd1d.onrender.com",
+]
 
 DEBUG = False   # Important for production
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com',
-                        'https://military-asset-management-system-beta.vercel.app',
-                        ]
+# CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com',
+#                         'https://military-asset-management-system-beta.vercel.app',
+#                         ]
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",), 
 }
+
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SAMESITE = "None"
+SESSION_COOKIE_SAMESITE = "None"
 
 
 # Internationalization
